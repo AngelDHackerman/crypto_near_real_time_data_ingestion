9b63aed01595172ba066790a63771e2374817e9d
Angel Hernandez <angeldariaux@gmail.com>
2025-09-19 21:30:23 -0600

    added versioning and SS3 to the raw bucket data

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,3 +1,4 @@
+# --- BRONZE ---
 resource "aws_s3_bucket" "lake_raw_data" {
   bucket = "lake-raw-data-bronze-${var.environment}"
 
@@ -9,6 +10,19 @@ resource "aws_s3_bucket" "lake_raw_data" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "lake_raw_data" {
+  bucket = aws_s3_bucket.lake_raw_data.id
+  versioning_configuration { status = "Enabled" }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "lake_raw_data" {
+  bucket = aws_s3_bucket.lake_raw_data.id
+  rule {
+    apply_server_side_encryption_by_default { sse_algorithm = "AES256" }
+  }
+}
+
+# --- CURATED (Silver/Gold) ---
 resource "aws_s3_bucket" "lake_curated_data" {
   bucket = "lake-curated-data-silver-gold-${var.environment}"
 
@@ -20,6 +34,7 @@ resource "aws_s3_bucket" "lake_curated_data" {
   }
 }
 
+# --- ARTIFACTS ---
 resource "aws_s3_bucket" "artifacts-crypto" {
   bucket = "artifacts-crypto-data-${var.environment}"
 
