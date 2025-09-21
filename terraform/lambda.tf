8904b1b3b6c875d45e513897d7dff0bb8c7f0538
Angel Hernandez <angeldariaux@gmail.com>
2025-09-21 07:38:40 -0600

    Added new basic role for lambda, full access to logs from lambda

---
+++ terraform/lambda.tf
@@ -0,0 +1,6 @@
+# resource "aws_lambda_function" "fetch_top10_crypto" {
+#   function_name         = "fetch-top10-crypto-${var.environment}"
+#   role                  = 
+# }
+
+# need to create IAM role. 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,19 @@ variable "environment" {
   description = "environment name"
   type = string
 }
+variable "secrets_manager_arn" {
+  description = "secrets manager"
+  type = string
+}
+variable "secrets_manager_name" {
+  description = "secrets manager name"
+  type = string
+}
+variable "bucket_lake_raw_name" {
+  description = "Lake Raw Bucket Name"
+  type = string
+}
+variable "bronze_prefix" {
+  description = "prefix for bronze data"  
+  type = string
+}
