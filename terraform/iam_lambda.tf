f621b45390e19a90d5f3d1bf413ff8c22608b275
Angel Hernandez <angeldariaux@gmail.com>
2025-09-22 19:52:00 -0600

    Created and attach IAM policy to lambda role for access S3 bucket

--- terraform/iam_lambda.tf
+++ terraform/iam_lambda.tf
@@ -14,3 +14,40 @@ resource "aws_iam_role_policy_attachment" "lambda_logs" {
   role              = aws_iam_role.lambda_role.name
   policy_arn        = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+# Give access to lambda to S3 Raw-Lake bucket
+data "aws_iam_policy_document" "lambda_s3_rw_bronze" {
+  # Put/Get/Head
+  statement {
+    sid = "S3ObjectsRWInBronzePrefix"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject"
+    ]
+    resources = [
+      "arn:aws:s3:::${var.bucket_lake_raw_name}/${var.bronze_prefix}/*"
+    ]
+  }
+  # List the bucket, limited to the bronze prefix
+  statement {
+    sid = "S3ListBucketBronzePrefixOnly"
+    actions = ["s3:ListBucket"]
+    resources = ["arn:aws:s3:::${var.bucket_lake_raw_name}"]
+
+    condition {
+      test = "StringLike"
+      variable = "s3:prefix"
+      values = ["${var.bronze_prefix}/*"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "lambda_s3_rw_bronze" {
+  name   = "lambda-s3-rw-${var.environment}-lake-raw-top10_bronze"
+  policy = data.aws_iam_policy_document.lambda_s3_rw_bronze.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_rw_attach" {
+  role        = aws_iam_role.lambda_role.name
+  policy_arn  = aws_iam_policy.lambda_s3_rw_bronze.arn
+}
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,21 +1,21 @@
-resource "aws_lambda_function" "fetch_top10_crypto" {
-  function_name         = "fetch-top10-crypto-${var.environment}"
-  role                  = aws_iam_role.lambda_role.arn
-  handler       = "app.handler"
-  runtime       = "python3.12"
-  filename      = "build/fetch_top10.zip"  # make sure to generate this file in the repo
-  timeout       = 120
-  memory_size   = 512
-  environment {
-    variables = {
-      RAW_BUCKET    = var.bucket_lake_raw_name
-      BRONZE_PREFIX = var.bronze_prefix
-      SECRET_ARN    = var.secrets_manager_arn
-      TOP_LIST_ID      = join(",", var.top10_list_id)
-    }
-  }
-}
-resource "aws_cloudwatch_log_group" "lambda_logs" {
-  name                  = "/aws/lambda/${aws_lambda_function.fetch_top10_crypto.function_name}"
-  retention_in_days     = 14
-}
+# resource "aws_lambda_function" "fetch_top10_crypto" {
+#   function_name         = "fetch-top10-crypto-${var.environment}"
+#   role                  = aws_iam_role.lambda_role.arn
+#   handler       = "app.handler"
+#   runtime       = "python3.12"
+#   filename      = "build/fetch_top10.zip"  # make sure to generate this file in the repo
+#   timeout       = 120
+#   memory_size   = 512
+#   environment {
+#     variables = {
+#       RAW_BUCKET    = var.bucket_lake_raw_name
+#       BRONZE_PREFIX = var.bronze_prefix
+#       SECRET_ARN    = var.secrets_manager_arn
+#       TOP_LIST_ID      = join(",", var.top10_list_id)
+#     }
+#   }
+# }
+# resource "aws_cloudwatch_log_group" "lambda_logs" {
+#   name                  = "/aws/lambda/${aws_lambda_function.fetch_top10_crypto.function_name}"
+#   retention_in_days     = 14
+# }
