d7383580a50df741c7231ceaad945bb5a0d4ef8b
Angel Hernandez <angeldariaux@gmail.com>
2025-10-04 11:01:51 -0600

    changed name of gold crawler, now is gold-features-base-crawlers

--- terraform/glue_catalog.tf
+++ terraform/glue_catalog.tf
@@ -53,13 +53,13 @@ resource "aws_glue_crawler" "silver_crawler" {
 
 # Crawler for Gold 
 resource "aws_glue_crawler" "gold_crawler" {
-  name            = "${var.project}-gold-crawler${var.environment}"
+  name            = "${var.project}-gold-feature-base-crawler-${var.environment}"
   role            = aws_iam_role.glue_crawler_role.arn
   database_name   = aws_glue_catalog_database.gold_db.name
   table_prefix    = "gold_"
 
   s3_target {
-    path        = "s3://${var.bucket_silver_gold_name}/${var.gold_prefix}/"
+    path        = "s3://${var.bucket_silver_gold_name}/${var.gold_features_prefix}/"
     exclusions  = [
       "**/manifest/**",
       "**/status/**",
