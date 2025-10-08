ed511a031db88229db63c89c9ceb603af5819763
Angel Hernandez <angeldariaux@gmail.com>
2025-10-07 20:48:13 -0600

    glue cralwer for ml_training table

--- terraform/glue_crawlers_catalog.tf
+++ terraform/glue_crawlers_catalog.tf
@@ -92,3 +92,45 @@ resource "aws_glue_crawler" "gold_crawler" {
 
   tags = var.tags
 }
+
+# Crawler for Gold ML Training features
+resource "aws_glue_crawler" "gold_crawler_ml" {
+  name            = "${var.project}-gold-ml-training-crawler-${var.environment}"
+  role            = aws_iam_role.glue_crawler_role.arn
+  database_name   = aws_glue_catalog_database.gold_db.name
+  table_prefix    = ""
+
+  s3_target {
+    path        = "s3://${var.bucket_silver_gold_name}/top10/gold/gold_ml_training/"
+    exclusions  = [
+      "**/manifest/**",
+      "**/status/**",
+      "**/_SUCCESS",
+      "**/.success",
+      "**/*.crc",
+      "**/*.json" 
+    ]
+  }
+
+  schema_change_policy {
+    update_behavior = "LOG"
+    delete_behavior = "LOG"
+  }
+
+  recrawl_policy {
+    recrawl_behavior = "CRAWL_NEW_FOLDERS_ONLY"
+  }
+
+  configuration = jsonencode({
+    Version = 1.0,
+    Grouping = { TableLevelConfiguration = 10 } # This will crate a single table for all cryptos
+    CrawlerOutput = {
+        Partitions = { AddOrUpdateBehavior = "InheritFromTable" }
+        Tables     = { AddOrUpdateBehavior = "MergeNewColumns" }
+    }
+  })
+
+  # schedule: will be done with stepFunctions
+
+  tags = var.tags
+}
