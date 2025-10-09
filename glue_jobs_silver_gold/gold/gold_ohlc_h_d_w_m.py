9464fe231472579fd9f64770b67be38b6f568715
Angel Hernandez <angeldariaux@gmail.com>
2025-10-09 17:01:05 -0600

    added base code for read silver layer and deduplication based on asset_id, event_time_utc and price_usd

--- glue_jobs_silver_gold/gold/gold_ohlc_h_d_w_m.py
+++ glue_jobs_silver_gold/gold/gold_ohlc_h_d_w_m.py
@@ -32,3 +32,25 @@ if grain not in valid:
 
 # -------- Read Silver --------
 df = spark.read.parquet(silver_path)
+
+# Defensive Casts
+df = (
+    df.withColumn("asset_id",         F.col("asset_id").cast(T.IntegerType()))
+      .withColumn("event_time_utc",   F.to_timestamp("event_time_utc"))
+      .withColumn("price_usd",        F.col("price_usd").cast(T.DoubleType()))
+      .withColumn("market_cap",       F.col("market_cap").cast(T.DoubleType()))
+      .withColumn("ingestion_ts_utc", F.to_timestamp("ingestion_ts_utc"))
+)
+
+# -------- DEDUP by (asset_id, event_time_utc, price_usd) --------
+w_dedupe = (
+    Window
+    .partitionBy("asset_id","event_time_utc","price_usd")
+    .orderBy(F.col("ingestion_ts_utc").desc_nulls_last(), F.col("event_time_utc").desc())
+)
+df = (df
+      .filter(F.col("asset_id").isNotNull() & F.col("event_time_utc").isNotNull() & F.col("price_usd").isNotNull())
+      .withColumn("rn_dedup", F.row_number().over(w_dedupe))
+      .filter(F.col("rn_dedup") == 1)
+      .drop("rn_dedup")
+)
