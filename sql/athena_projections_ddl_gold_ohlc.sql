57944ba6b00904d08ed9f3c5ecc0f658c8c27d42
Angel Hernandez <angeldariaux@gmail.com>
2025-10-09 19:29:49 -0600

    added sql code for create views of hour, day, week and month

--- sql/athena_projections_ddl_gold_ohlc.sql
+++ sql/athena_projections_ddl_gold_ohlc.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE IF NOT EXISTS crypto_gold_db;
 
 CREATE EXTERNAL TABLE IF NOT EXISTS crypto_gold_db.ohlc (
-  asset_id            int,
   period_start        timestamp,
   start_ts            timestamp,
   end_ts              timestamp,
@@ -17,34 +16,39 @@ CREATE EXTERNAL TABLE IF NOT EXISTS crypto_gold_db.ohlc (
   close_market_cap    double
 )
 PARTITIONED BY (
-  g  string,  -- hour|day|week|month
+  g  string,
   dt date,
   asset_id int
 )
 STORED AS PARQUET
 LOCATION 's3://lake-curated-data-silver-gold-crypto/top10/gold_ohlc/'
 TBLPROPERTIES (
-  -- Habilita Partition Projection
   'projection.enabled' = 'true',
-
-  -- g: enum con tus 4 granularidades
   'projection.g.type'    = 'enum',
   'projection.g.values'  = 'hour,day,week,month',
-
-  -- dt: rango de fechas dinámico (ajusta la fecha inicial a tu historico real)
   'projection.dt.type'          = 'date',
   'projection.dt.format'        = 'yyyy-MM-dd',
   'projection.dt.interval'      = '1',
   'projection.dt.interval.unit' = 'DAYS',
-  -- Comienza cuando arrancó tu pipeline; 'NOW' cierra en tiempo de consulta
   'projection.dt.range'         = '2025-09-01,NOW',
-
-  -- asset_id: rango entero (ajústalo a tus IDs reales)
   'projection.asset_id.type'  = 'integer',
   'projection.asset_id.range' = '1,999999',
   'projection.asset_id.digits' = '1',
-
-  -- Template que mapea las particiones a la ruta S3 real
   'storage.location.template' =
     's3://lake-curated-data-silver-gold-crypto/top10/gold_ohlc/g=${g}/dt=${dt}/asset_id=${asset_id}/'
 );
+
+
+-- Create the views
+
+CREATE OR REPLACE VIEW crypto_gold_db.gold_ohlc_hour  AS
+SELECT * FROM crypto_gold_db.gold_ohlc WHERE g='hour';
+
+CREATE OR REPLACE VIEW crypto_gold_db.gold_ohlc_day   AS
+SELECT * FROM crypto_gold_db.gold_ohlc WHERE g='day';
+
+CREATE OR REPLACE VIEW crypto_gold_db.gold_ohlc_week  AS
+SELECT * FROM crypto_gold_db.gold_ohlc WHERE g='week';
+
+CREATE OR REPLACE VIEW crypto_gold_db.gold_ohlc_month AS
+SELECT * FROM crypto_gold_db.gold_ohlc WHERE g='month';
