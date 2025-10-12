428e56a78cd57e736e240eccc3341781aab2aadd
Angel Hernandez <angeldariaux@gmail.com>
2025-10-11 20:37:52 -0600

    added SNS notification in case eventBridge fails

--- terraform/sns_failed.tf
+++ terraform/sns_failed.tf
@@ -1,4 +1,5 @@
-# SNS topic + suscripción
+# If StateFunctions run fails, send alert about failure
+# 1) SNS topic + suscripción
 resource "aws_sns_topic" "sfn_alerts" {
   name = "near-real-time-crypto-sfn-alerts-${var.environment}"
 }
@@ -6,5 +7,47 @@ resource "aws_sns_topic" "sfn_alerts" {
 resource "aws_sns_topic_subscription" "sfn_alerts_email" {
   topic_arn = aws_sns_topic.sfn_alerts.arn
   protocol  = "email"
-  endpoint  = "tu-correo@ejemplo.com" # <-- cambia esto
+  endpoint  = var.sns_email
+}
+
+# Permitir a EventBridge publicar en el topic
+data "aws_iam_policy_document" "sns_topic_policy" {
+  statement {
+    sid    = "AllowEventsToPublish"
+    effect = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+    actions   = ["SNS:Publish"]
+    resources = [aws_sns_topic.sfn_alerts.arn]
+  }
+}
+resource "aws_sns_topic_policy" "sfn_alerts_policy" {
+  arn    = aws_sns_topic.sfn_alerts.arn
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+#2) EventBridge rule that detects failed state machine executions
+resource "aws_cloudwatch_event_rule" "sfn_failed" {
+  name        = "near-real-time-crypto-sfn-failed-${var.environment}"
+  description = "Notifica si la ejecución del Step Functions falla/timeout/abort"
+  event_pattern = jsonencode({
+    "source" : ["aws.states"],
+    "detail-type" : ["Step Functions Execution Status Change"],
+    "detail" : {
+      "stateMachineArn" : [aws_sfn_state_machine.daily_gold_pipeline.arn],
+      "status" : [
+        "FAILED",
+        "TIMED_OUT",
+        "ABORTED"
+      ]
+    }
+  })
+}
+
+# 3) Target: SNS
+resource "aws_cloudwatch_event_target" "sfn_failed_to_sns" {
+  rule = aws_cloudwatch_event_rule.sfn_failed.name
+  arn  = aws_sns_topic.sfn_alerts.arn
 }
